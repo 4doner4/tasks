83e6492be6f56b6faf1f0a06616a6e2a9025738a
Doner <damir.ametov34@gmai.com>
2024-07-30 21:29:40 +0500

    5 from 6

---
+++ TestProject/BusyMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    /*
+    Есть метод, к которому обращается много пользователей
+    public Task SomeBusyMethod()
+    В один момент времени к методу может обратиться не более 3 пользователей. Опишите метод
+    */
+
+    // немного не понял задачу 
+    class BusyMethod
+    {
+        private int connectCount = 0;
+        public Task SomeBusyMethod()
+        {
+
+            connectCount++;
+            if (connectCount > 2)
+            {
+                return new Task(() => {
+                    Console.WriteLine("3 connect");
+                    });
+            }
+            Task task = new (() => {
+            });
+            task.Start();
+            connectCount --;
+            return task;
+        }
+    }
+}
---
+++ TestProject/Hierarchy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    /*
+Есть класс
+class
+{
+	public int Id {get;set;}
+	public string Name {get;set;}
+	public int? ParentId {get;set;}
+}
+
+Набор данных представляет собой плоскую коллекцию с n кол-вом данных. У данных есть связь по ParentId (родитель-ребенок)
+Написать алгоритм (метод) который превратит плоскую структуру в иерархическую
+
+    */
+    class Hierarchy
+    {
+        public static void ShowHierarchy()
+        {
+            Hierarchy hierarchy = new Hierarchy();
+        }
+    }
+
+    class Root
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+    }
+
+}
